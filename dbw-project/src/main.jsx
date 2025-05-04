69e3796d93ef83fb46e245805377d518f828a899
TiagoAlves74 <162820770+TiagoAlves74@users.noreply.github.com>
2025-05-04 21:07:57 +0100

    Update main.jsx

--- dbw-project/src/main.jsx
+++ dbw-project/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './containers/Login/LoginSignup.jsx'
+import Time_theme from './containers/select_time_theme/time_theme.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <Time_theme />
+    {/* <Login /> */}
   </StrictMode>,
 )
